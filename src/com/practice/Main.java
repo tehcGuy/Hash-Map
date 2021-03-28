48b7727eb1533f8f3682faac90d80d408c4523e0
max <scraptrashscrap@gmail.com>
2021-03-28 10:59:25 +0200

    Initial commit

---
+++ src/com/practice/Main.java
@@ -0,0 +1,16 @@
+package com.practice;
+
+
+public class Main {
+
+    static HashM_pplCharecteristics presidents = new HashM_pplCharecteristics();
+    static HashM_pplCharecteristics presidents1 = new HashM_pplCharecteristics();
+
+
+    public static void main(String[] args) {
+        presidents.put("Biden", "Liberal");
+        presidents.put("Biden", "Liberal");
+//        System.out.println(presidents1.getPeopleCharecteristics());
+
+    }
+}
