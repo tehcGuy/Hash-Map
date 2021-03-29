7621d9c269c61033df5d7ca4e0ed7e1b8004b516
max <scraptrashscrap@gmail.com>
2021-03-29 09:52:14 +0200

    Fixing methods to count fields

---
+++ src/com/practice/HashM_pplCharacteristics.java
@@ -0,0 +1,62 @@
+package com.practice;
+
+import java.lang.reflect.Field;
+import java.util.*;
+
+public class HashM_pplCharacteristics extends HashMap<String, String> {
+    private static int countsInstanceNames;
+    private int fieldPosition = -1;
+
+    private void givesInstancePosition(){ // grouping two fields in order to prevent an accident modification
+        HashM_pplCharacteristics.countsInstanceNames++;
+        this.fieldPosition += HashM_pplCharacteristics.countsInstanceNames;
+    }
+
+    private final Map<String, String> peopleCharacteristics;
+
+    public HashM_pplCharacteristics() {
+        this.peopleCharacteristics = new HashMap<>();
+        givesInstancePosition();
+    }
+
+    public Map<String, String> getPeopleCharacteristics() {
+        System.out.println(getTheInstanceName());
+        return peopleCharacteristics;
+    }
+
+    // making put() adding a unique key
+    @Override
+    public String put(String key, String value) { // I cannot change the return type
+        if(!this.peopleCharacteristics.containsKey(key)) {
+            this.peopleCharacteristics.put(key, value);
+            System.out.print(key + "  --->  added");
+        } else {
+            System.out.print("\nSorry. Key is within the Hash Map");
+        }
+        return ""; //I have to return an object, or a String object
+    }
+
+    @Override
+    public String remove(Object key) {
+        return super.remove(key);
+    }
+
+    @Override
+    public String toString() {
+        return "HashM_pplCharacteristics{" + "peopleCharacteristics=" + peopleCharacteristics + '}';
+    }
+
+    private String getTheInstanceName() {
+        Field[] declaredFields = Main.class.getDeclaredFields();
+            Field f = declaredFields[fieldPosition];
+                return "\nField name: " + f.getName();
+    }
+    /* ITERATING THROUGH THE FIELDS
+    private static String getTheInstanceName() {
+        for(Field f : Main.class.getDeclaredFields()) {
+            if(f.getType().equals(HashM_pplCharacteristics.class))//Here you would retrieve the variable name when the type is dieselEngine.
+                return f.getName();
+        }
+        return null; //actually, it will never happen
+    }*/
+}
--- src/com/practice/Main.java
+++ src/com/practice/Main.java
@@ -1,16 +1,30 @@
 package com.practice;
+// Add the unique keys into the HashMap, and throw the message if the key already exists
+// Prints the object names
 
-
+/*TASKS
+* 1. Polish semantics
+* 2. ?Modify for loop
+* 3. Rename class to HASHMAP presidents*/
 public class Main {
 
-    static HashM_pplCharecteristics presidents = new HashM_pplCharecteristics();
-    static HashM_pplCharecteristics presidents1 = new HashM_pplCharecteristics();
+    static HashM_pplCharacteristics presidents = new HashM_pplCharacteristics();
+    static HashM_pplCharacteristics europePresidents = new HashM_pplCharacteristics();
+    static HashM_pplCharacteristics test = new HashM_pplCharacteristics();
 
 
     public static void main(String[] args) {
         presidents.put("Biden", "Liberal");
-        presidents.put("Biden", "Liberal");
-//        System.out.println(presidents1.getPeopleCharecteristics());
+        System.out.println(presidents.put("Biden", "US"));
+        System.out.println(presidents.put("Biden", "Liberal"));
+
+        System.out.println(europePresidents.put("Andrzej Duda", "Poland"));
+
+        test.put("Kim Jin Ung", "NK");
+
+        System.out.println(test.getPeopleCharacteristics());
+        System.out.println(presidents.getPeopleCharacteristics());
+        System.out.println(europePresidents.getPeopleCharacteristics());
 
     }
 }
